21044ad15509ccd652a183410857433349d6500f
toivomattila <toivo.m@hotmail.fi>
2018-02-18 12:35:54 +0200

    Cleaned code

--- app.py
+++ app.py
@@ -4,23 +4,14 @@ import time
 import thread
 import sys
 
-#TODO
-#Two-way communication?
-#IP from command line
-
 def client(host):
-    print(host)
     s = socket.socket()
     port = 12345
     s.connect((host, port))
-    clipboard = pyperclip.paste()
     while True:
-        tmp = s.recv(1024)
-        if(tmp != clipboard and tmp != ""): #tmp != pyperclip.paste()
-            clipboard = tmp #remove
-            print(tmp)
-            pyperclip.copy(clipboard) #(tmp)
-            print("updated " + clipboard)
+        data = s.recv(65536)
+        if(data != pyperclip.paste() and data != ""):
+            pyperclip.copy(data)
         time.sleep(1)
     s.close
 
@@ -30,7 +21,8 @@ def server(client_ip):
         thread.start_new_thread(client, (client_ip,))
     s = socket.socket()
     host = socket.gethostname()
-    host = '192.168.10.33'
+    #host = '192.168.10.33'
+    print('Listening at ' + host)
     port = 12345
     s.bind((host, port))
 
@@ -38,17 +30,16 @@ def server(client_ip):
     c, addr = s.accept()
     #client
     if not client_ip:
-        print("Client starting")
         thread.start_new_thread(client, (addr[0],))
-        print("Client started")
-    print("Server 9")
     clipboard = ""
     while True:
         if(clipboard != pyperclip.paste()):
             clipboard = pyperclip.paste()
             c.send(clipboard)
-            print("sent " + clipboard)
         time.sleep(1)
     c.close()
 
-server('')
+client_ip = ''
+if len(sys.argv) == 2:
+    client_ip = sys.argv[1]
+server(client_ip)
